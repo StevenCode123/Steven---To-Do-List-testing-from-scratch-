6eb78a533212b8a513257c27235b2c06684bd512
Steven100221 <117389635+Steven10012@users.noreply.github.com>
2026-06-10 10:09:13 -0400

    New file changes test

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 "# Steven---To-Do-List-testing-from-scratch-" 
 To-Do List
+
+-- To-Do List rendered via:
+--functions
+--template strings
+--buttons
+--innerHTML
+--createElement
+--localstorage
--- index.js
+++ index.js
@@ -9,55 +9,50 @@ let ulEl = document.getElementById("ul-el");
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
+var listItemCheckbox = document.createElement('input')
+listItemCheckbox.type = 'checkbox';
+
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
 
+//renderleads function using parameters
 function render(leads) {
   let defaultLeads = "";
   for (let i = 0; i < leads.length; i++) {
-    defaultLeads += `<li class = "list"> 
-            ${leads[i]} 
+    //rendering defaultLeads using innerHTML.textCONTENT & template strings
+    defaultLeads += `<li class = "list">
+            ${leads[i]}
         </li>`;
   }
   ulEl.innerHTML = defaultLeads;
 }
 
+//recreate the renderleads function
+// using createElement and appendChild instead of the classic "innerHTML"
+
 clearBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   //clearing the router DOM
   console.log(myLeads);
-  ulEl.textContent = ""
-  myLeads = []
-  console.log(leads)
+  ulEl.textContent = "";
+  myLeads = [];
+  console.log(leads);
 });
-
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  inputEl.value = ""
-  console.log(myLeads)
+  inputEl.value = "";
+  console.log(myLeads);
   render(myLeads);
 });
 
-document.getElementById('input-el'), function (e) {
-  myLeads.push(inputEl.value);
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  inputEl.value = "";
-  console.log(myLeads);
-  renderLeads();
-});
-
-
-function buttonCode(){
-  alert("Button clicked")
+function buttonCode() {
+  alert("Button clicked");
 }
-
-
 
 //TODO
 //1. Adding a checkbox beside array items itself
 //2. "Enter" button mechanic for user input
-
